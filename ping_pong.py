65cecf107b761f60846f8db6b334e41ad1ceee47
volodymyrlogika <95148713+volodymyrlogika@users.noreply.github.com>
2022-10-12 16:16:43 +0300

    Update ping_pong.py
    Доданий клас GameSprite. Введений новий спрайт ball. При запуску відображається м'яч на сцені.

--- ping_pong.py
+++ ping_pong.py
@@ -14,20 +14,7 @@ class GameSprite(sprite.Sprite):
    def reset(self):
        window.blit(self.image, (self.rect.x, self.rect.y))
  
-class Player(GameSprite):
-   def update_r(self):
-       keys = key.get_pressed()
-       if keys[K_UP] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_DOWN] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
-   def update_l(self):
-       keys = key.get_pressed()
-       if keys[K_w] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_s] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
- 
+
 #ігрова сцена:
 back = (200, 255, 255)  #колір фону (background)
 win_width = 600
@@ -41,9 +28,6 @@ finish = False
 clock = time.Clock()
 FPS = 60
  
-#створення м'яча та ракетки  
-racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
-racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
  
 font.init()
@@ -61,33 +45,7 @@ while game:
   
     if finish != True:
         window.fill(back)
-        racket1.update_l()
-        racket2.update_r()
-        ball.rect.x += speed_x
-        ball.rect.y += speed_y
-    
-        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
-            speed_x *= -1
-            speed_y *= 1
         
-        #якщо м'яч досягає меж екрана, змінюємо напрямок його руху
-        if ball.rect.y > win_height-50 or ball.rect.y < 0:
-            speed_y *= -1
-    
-        #якщо м'яч відлетів далі ракетки, виводимо умову програшу для першого гравця
-        if ball.rect.x < 0:
-            finish = True
-            window.blit(lose1, (200, 200))
-            game_over = True
-    
-        #якщо м'яч полетів далі ракетки, виводимо умову програшу другого гравця
-        if ball.rect.x > win_width:
-            finish = True
-            window.blit(lose2, (200, 200))
-            game_over = True
-    
-        racket1.reset()
-        racket2.reset()
         ball.reset()
     
     display.update()
